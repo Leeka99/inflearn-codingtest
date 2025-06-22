03006129d651681401c6670dcd91037495324b21
Leeka99 <dsrn0000@gmail.com>
2025-06-23 00:46:48 +0900

    중복 문자 제거

---
+++ 중복문자제거/Main.java
@@ -0,0 +1,24 @@
+package 중복문자제거;
+
+import java.util.Scanner;
+
+// .indexOf()를 활용해서 해결하자. indexOf()는 마지막의 위치를 반환한다.
+// -> 현재위치 == .indexOf() -> 위치가 같다면 중복없음.
+// -> 현재위치 != .indexOf() -> 위치가 다르다면 중복있음.
+
+public class Main {
+  public static void main(String[] args){
+    Scanner in=new Scanner(System.in);
+    String input1 = in.next();
+    int num = input1.length();
+
+    String answer = "";
+    for (int i = 0; i < num; i++) {
+      if (i == input1.indexOf(input1.charAt(i))) {
+        answer += input1.charAt(i);
+      }
+    }
+
+    System.out.println(answer);
+  }
+}
